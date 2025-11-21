8bd590a5a7b5c630762d97bc48fbea641e3386fd
Kelvin <kelvinchauvel@gmail.com>
2025-11-21 11:52:13 +0100

    <Commit> 3eme Micro service
    taxonomy-service est en place

---
+++ taxonomy-service/src/index.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+const OBSERVATION_SERVICE_URL = process.env.OBSERVATION_SERVICE_URL || 'http://localhost:3002';
+
+async function fetchData() {
+  try {
+
+    const speciesResp = await axios.get(`${OBSERVATION_SERVICE_URL}/species/`);
+    const species = speciesResp.data;
+
+    const observationsResp = await axios.get(`${OBSERVATION_SERVICE_URL}/observations/get`);
+    const observations = observationsResp.data;
+
+    return { species, observations };
+  } catch (err) {
+    console.error("Erreur lors de la récupération des données :", err.message);
+    return { species: [], observations: [] };
+  }
+}
